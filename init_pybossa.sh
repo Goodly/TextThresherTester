1cc6778f30f6f34afc54f586866bccf07c15a45f
Norman Gilmore <git@teamforecast.com>
2017-06-19 12:55:36 -0700

    Add MSYS_NO_PATHCONV=1 to init scripts to fix path breakage by Docker Toolbox-Mingw shell.

--- init_pybossa.sh
+++ init_pybossa.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# MSYS_NO_PATHCONV fixes paths on Docker Toolbox on Windows using Git Bash / Mingw
+# Harmless everywhere else.
+export MSYS_NO_PATHCONV=1
 docker-compose run pybossa sh /ansible_build/init_db.sh
--- reset_pybossa.sh
+++ reset_pybossa.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# MSYS_NO_PATHCONV fixes paths on Docker Toolbox on Windows using Git Bash / Mingw
+# Harmless everywhere else.
+export MSYS_NO_PATHCONV=1
 docker-compose exec pybossa supervisorctl stop pybossa
 docker-compose exec db sh /scripts/reset_pybossa_db.sh
 docker-compose exec pybossa supervisorctl start pybossa
