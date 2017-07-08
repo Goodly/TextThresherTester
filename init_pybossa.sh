bb011ce88cacfd1d6517cbb48e0d7a85e9e8cc34
Norman Gilmore <git@teamforecast.com>
2017-07-07 21:03:31 -0700

    Update Pybossa init script to set proper container user.

--- init_pybossa.sh
+++ init_pybossa.sh
@@ -2,7 +2,4 @@
 # MSYS_NO_PATHCONV fixes paths on Docker Toolbox on Windows using Git Bash / Mingw
 # Harmless everywhere else.
 export MSYS_NO_PATHCONV=1
-docker-compose exec pybossa supervisorctl stop pybossa
-docker-compose exec db sh /scripts/reset_pybossa_db.sh
-docker-compose exec pybossa supervisorctl start pybossa
 docker-compose exec --user pybossa pybossa sh /ansible_build/init_db.sh
